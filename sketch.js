c96fd93de1913b2fad828b3386116c06c88e764b
Alissa Diaz <adiaz9@lsu.edu>
2016-08-25 11:31:14 -0500

    wip

---
+++ sketch.js
@@ -0,0 +1,37 @@
+function setup() {
+  createCanvas(500, 500);
+  ellipse(0, 250, 50);
+  ellipse(20, 250, 70, 30)
+  ellipse(40, 250, 30, 90)
+  ellipse(60, 250, 40, 50)
+  ellipse(80, 250, 50, 70)
+  ellipse(100, 250, 60, 80)
+  ellipse(120, 250, 40, 100)
+  ellipse(160, 250, 100,100)
+  ellipse(200, 250, 120, 150)
+  ellipse(210, 250, 140, 180)
+  ellipse(250, 250, 200)
+  ellipse(270, 250, 150, 120)
+  ellipse (290, 250, 130, 120)
+  ellipse(310, 250, 120, 100)
+  ellipse(330, 250, 90, 110)
+  ellipse(360, 250, 50, 100)
+  ellipse(380, 250, 40, 60)
+  ellipse(400, 250, 80, 100)
+  ellipse(420, 250, 70, 80)
+  ellipse(440, 250, 50, 70)
+  ellipse(460, 250, 90, 100)
+  ellipse(480, 250, 60, 140)
+  ellipse(500, 250, 50)
+  ellipse(500, 0, 100)
+  ellipse(460, 20, 70, 60)
+  ellipse(440, 30, 80, 60)
+  ellipse(430, 40, 30, 100)
+  ellipse(410, 50, 90, 60)
+  ellipse(405, 55, 70, 40)
+  ellipse(390, 65, 90, 50)
+
+
+
+
+}
